750f0c3a44d4bab7a67793f8152846813d7a9e83
Jolanta Jasiulinyte <klpdjolanta@gmail.com>
2017-11-03 12:55:12 +0000

    added new journeylog class and rspec files

--- lib/journeylog.rb
+++ lib/journeylog.rb
@@ -1,14 +1,30 @@
 require 'journey'
 
-class JourneyLog
-  attr_reader :journey_history, :journey
-
-  def initialize(journey_class = Journey)
-    @journey_history = []
-    @journey_class = journey_class
-  end
-
-  def start(entry_station)
-    @journey = @journey_class.new(entry_station: entry_station)
-  end
+class Journeylog
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class JourneyLog
+#   attr_reader :journey_history, :journey
+#
+#   def initialize(journey_class = Journey)
+#     @journey_history = []
+#     @journey_class = journey_class
+#   end
+#
+#   def start(entry_station)
+#     @journey = @journey_class.new(entry_station: entry_station)
+#   end
+# end
--- spec/journeylog_spec.rb
+++ spec/journeylog_spec.rb
@@ -1,32 +1,61 @@
 require 'journeylog'
 
-describe JourneyLog do
-  let(:journey) { double(:journey) }
-  let(:journey_class) { double(:journey_class, :new => journey) }
-  let(:station) { double(:station) }
-  subject { described_class.new(journey_class) }
+describe Journeylog do
 
-  describe "#initialize" do
-    it "should return an instance of JourneyLog" do
-      expect(subject).to be_an_instance_of(JourneyLog)
-    end
-    it "creates an empty journey_history" do
-      expect(subject.journey_history).to be_a(Array)
-        .and be_empty
-    end
-    context "when creating new journey_log" do
-      subject { described_class }
 
-      it "takes one arg upon instantiation" do
-        is_expected.to respond_to(:new).with(1).arguments
-      end
-    end
-  end
-
-  describe '#start' do
-    it 'creates a new Journey' do
-      expect(subject.start(station)).to eq(journey)
+  describe '#initialize' do
+    it 'should be an instance of journeylog class' do
+      expect(subject).to be_an_instance_of(Journeylog)
     end
   end
 
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# require 'journeylog'
+#
+# describe JourneyLog do
+#   let(:journey) { double(:journey) }
+#   let(:journey_class) { double(:journey_class, :new => journey) }
+#   let(:station) { double(:station) }
+#   subject { described_class.new(journey_class) }
+#
+#   describe "#initialize" do
+#     it "should return an instance of JourneyLog" do
+#       expect(subject).to be_an_instance_of(JourneyLog)
+#     end
+#     it "creates an empty journey_history" do
+#       expect(subject.journey_history).to be_a(Array)
+#         .and be_empty
+#     end
+#     context "when creating new journey_log" do
+#       subject { described_class }
+#
+#       it "takes one arg upon instantiation" do
+#         is_expected.to respond_to(:new).with(1).arguments
+#       end
+#     end
+#   end
+#
+#   describe '#start' do
+#     it 'creates a new Journey' do
+#       expect(subject.start(station)).to eq(journey)
+#     end
+#   end
+#
+# end
